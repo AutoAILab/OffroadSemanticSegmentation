101bb82deded6a1df9c1b6f707d111aefe9d6250
nobeldang <nobeldang@gmail.com>
2024-10-16 23:22:47 -0400

    restructring clipself

--- CLIPSelf/scripts/test_eva_vitl14_macc_boxes_masks.sh
+++ CLIPSelf/scripts/test_eva_vitl14_macc_boxes_masks.sh
@@ -1,6 +1,6 @@
 NAME=$1
 CHECKPOINT=$2
-torchrun --nproc_per_node 8 -m training.main --batch-size=1 \
+torchrun --nproc_per_node 2 -m training.main --batch-size=1 \
 --model EVA02-CLIP-L-14-336 --pretrained eva --test-type coco_panoptic --train-data="" \
 --val-data data/coco/annotations/panoptic_val2017.json \
 --embed-path metadata/coco_panoptic_clip_hand_craft_EVACLIP_ViTL14x336.npy \
